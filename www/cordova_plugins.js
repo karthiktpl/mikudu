7fb93c8d99a495841cf3961b95205af1b1f457c9
karthiktn <karthik@solminds.com>
2016-09-21 16:11:33 +0530

    Signed-off-by: karthiktn <karthik@solminds.com>

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,14 +1,36 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
+module.exports = [    
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    },
+	{
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{    
+    "cordova-plugin-geolocation": "2.1.0",
+	"plugin.google.maps": "1.3.9"
+}
 // BOTTOM OF METADATA
 });
