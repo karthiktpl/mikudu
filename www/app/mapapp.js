965dcc61832d861d0000170140049bea6003c0ae
karthiktn <karthik@solminds.com>
2016-09-22 20:01:41 +0530

    Signed-off-by: karthiktn <karthik@solminds.com>

--- www/app/mapapp.js
+++ www/app/mapapp.js
@@ -6,6 +6,45 @@ app.config(function ($httpProvider) {
   $httpProvider.defaults.headers.put = {};
   $httpProvider.defaults.headers.patch = {};
     
-});
+}).run(function ($rootScope, $location, Data) {
+        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+            $rootScope.authenticated = true;
+            var storage = window.localStorage; 
+
+            if(storage.getItem('uid'))
+            {
+                $rootScope.uid = storage.getItem('uid');
+                $rootScope.name = storage.getItem('name');
+                $rootScope.email = storage.getItem('email');
+                $rootScope.district = storage.getItem('district');                 
+            }
+            else
+            {
+                    var nextUrl = next.$$route.originalPath;
+                    if (nextUrl == '/signup' || nextUrl == '/login') {
+
+                    } else {
+                        $location.path("/login");
+                    }                
+            }          
+            /*Data.get('session').then(function (results) {
+                if (results.uid) {
+                    $rootScope.authenticated = true;
+                    $rootScope.uid = results.uid;
+                    $rootScope.name = results.name;
+                    $rootScope.email = results.email;
+                    $rootScope.district = results.district;
+                    $rootScope.notifications = results.notifications;                     
+                } else {
+                    var nextUrl = next.$$route.originalPath;
+                    if (nextUrl == '/signup' || nextUrl == '/login') {
+
+                    } else {
+                        $location.path("/login");
+                    }
+                }
+            });*/
+        });
+    });;
 
         
